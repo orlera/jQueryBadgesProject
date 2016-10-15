60a9da7f03b6366352910cd6cb26bd29594f6131
DESKTOP-CDK3R3P\andrea.orler <andreaorler@gmail.com>
2016-10-15 17:41:36 +0200

    First commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,46 @@
 $(function() {
+	$.ajax(
+	{
+	    url: 'https://www.codeschool.com/users/2067316.json',
+	    dataType: 'jsonp',
+	    success: function(response)
+	    {
+	    	displayBadges(response.courses.completed);
+	    }
+	});
+  
+	var displayBadges = function (badges)
+	{
+		badges.sort(compareBadgesByTitle);
+		$(badges).each(function(index) {
+			jQuery('<div/>', {
+			    class: 'course'
+			}).appendTo("#badges");
+		});
 
-  // your code will go here
+		$(".course").each(function(index) {
+			jQuery('<h3/>', {
+				text: badges[index].title
+			}).appendTo(this);
+			jQuery('<img/>', {
+				'src': badges[index].badge
+			}).appendTo(this);
+			jQuery('<a/>', {
+				'href': badges[index].url,
+				'target': '_blank',
+				'text': 'See Course'
+			}).addClass("btn btn-primary").appendTo(this);
+		});
+	};
 
+	var compareBadgesByTitle = function (a, b)
+	{
+		if (a.title < b.title) {
+			return -1;
+		}
+		if (a.title > b.title) {
+			return 1;
+		}
+		return 0;
+	}
 });
